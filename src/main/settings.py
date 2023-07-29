73ed9181a3f98bcba986c209a9012b1d6cac8190
HIEP <DH>
2023-07-29 18:23:06 +0700

    Add post app

--- src/main/settings.py
+++ src/main/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     # 3rd parties
     "rest_framework",
     "drf_spectacular",
-    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -143,7 +142,4 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 1000,
-    "DEFAULT_FILTER_BACKENDS": [
-        "django_filters.rest_framework.DjangoFilterBackend",
-    ],
 }
